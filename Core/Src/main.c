5d3d442826bec23392e27209ec20ff7e51b1187c
Jorge Larach <jorgelarachesp@gmail.com>
2026-01-18 13:31:28 -0600

    Got blinky working on FreeRTOS

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -296,7 +296,8 @@ void StartTask1(void *argument)
   /* Infinite loop */
   for(;;)
   {
-    osDelay(1);
+    HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+    osDelay(1000);
   }
   /* USER CODE END 5 */
 }
