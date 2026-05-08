b679667c740771f9c2726098b5cc1a0cdaf474af
Jorge Larach <jorgelarachesp@gmail.com>
2026-05-08 00:12:23 -0500

    f446-port now targets and builds. will begin porting main branch code now

---
+++ build_rust_solver.sh
@@ -0,0 +1,25 @@
+# #!/bin/sh
+# set -e
+
+# echo "[Rust] Building cube_solver..."
+
+# cd cube_solver
+
+# /Users/jorgelarach/.cargo/bin/cargo rustc \
+# --target thumbv7em-none-eabihf \
+# --release \
+# --no-default-features \
+# --crate-type staticlib \
+# --lib
+
+# echo "[Rust] Copying static library..."
+
+# cp target/thumbv7em-none-eabihf/release/libcube_solver.a \
+# ../Middlewares/cube_solver/libcube_solver.a
+
+# echo "[Rust] Cleaning Cargo..."
+
+# /Users/jorgelarach/.cargo/bin/cargo clean
+
+# echo "[Rust] Done."
+
