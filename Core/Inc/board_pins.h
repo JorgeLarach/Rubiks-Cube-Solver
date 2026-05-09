92c93277235dd4932724eff56742ca8e5bdb08a7
Jorge Larach <jorgelarachesp@gmail.com>
2026-05-09 02:50:48 -0500

    attempt 2 to fix stash issue

--- Core/Inc/board_pins.h
+++ Core/Inc/board_pins.h
@@ -13,8 +13,13 @@
 #define U_STEP_GPIO_Port GPIOA
 #define U_STEP_Pin  GPIO_PIN_0
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 #define U_DIR_GPIO_Port  GPIOC
 #define U_DIR_Pin   GPIO_PIN_2
+=======
+#define U_DIR_GPIO_Port  GPIOA
+#define U_DIR_Pin   GPIO_PIN_1
+>>>>>>> Stashed changes
 =======
 #define U_DIR_GPIO_Port  GPIOA
 #define U_DIR_Pin   GPIO_PIN_1
